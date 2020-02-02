c3c86137524d0f67edff69b0878172bd9fcde6cf
Abdiwahab Ahmed <abdiwahabahmed@Abdiwahabs-MacBook-Air.local>
2020-02-02 16:42:37 -0500

    fixed functions

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const inquirer = require("inquirer");
 const mysql = require("mysql");
 const Functions = require("./functions");
-const myFunctions = new Functions();
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -13,62 +11,5 @@ const connection = mysql.createConnection({
 
 connection.connect(() => {
   console.log(`connected to database ${connection.threadId}`);
-  options();
+  Functions.options();
 });
-
-function options() {
-  inquirer
-    .prompt({
-      name: "options",
-      type: "list",
-      choices: [
-        "add a department",
-        "add a role",
-        "add an employee",
-        "view departments",
-        "view roles",
-        "view employees",
-        "update employees",
-        "update roles",
-        "exit"
-      ]
-    })
-    .then(ans => {
-      switch (ans.options) {
-        case "add a department":
-          myFunctions.addDepartment();
-          break;
-
-        case "add a role":
-          addRole();
-          break;
-
-        case "add an employee":
-          addEmployee();
-          break;
-
-        case "view departments":
-          myFunctions.viewDepartments();
-          break;
-
-        case "view roles":
-          viewRoles();
-          break;
-
-        case "view employees":
-          viewEmployees();
-          break;
-
-        case "update employees":
-          updateEmployees();
-          break;
-
-        case "update roles":
-          updateRoles();
-          break;
-        case "exit":
-          connection.end();
-          break;
-      }
-    });
-}
--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@ const ctable = require("console.table");
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
-const conn = mysql.createPool({
+const db = mysql.createPool({
   host: "localhost",
   user: "root",
   port: 3306,
@@ -12,183 +12,242 @@ const conn = mysql.createPool({
 
 class Functions {
   constructor() {}
-  addDepartment() {
+  options() {
     inquirer
       .prompt({
-        name: "name",
-        type: "input",
-        message: "add a department"
+        name: "options",
+        type: "list",
+        choices: [
+          "add a department",
+          "add a role",
+          "add an employee",
+          "view departments",
+          "view roles",
+          "view employees",
+          "update employees",
+          "update roles",
+          "exit"
+        ]
       })
-      .then(function(res) {
-        db.query("INSERT INTO department (name) VALUES ?", res.name, function(
-          err
-        ) {
-          if (err) throw err;
-          options();
-        });
-      });
-  }
+      .then(ans => {
+        switch (ans.options) {
+          case "add a department":
+            inquirer
+              .prompt({
+                name: "name",
+                type: "input",
+                message: "add a department"
+              })
+              .then(function(res) {
+                db.query(
+                  "INSERT INTO department (name) VALUE (?)",
+                  res.name,
+                  function(err) {
+                    if (err) throw err;
+                    let user = new Functions();
+                    user.options();
+                  }
+                );
+              });
 
-  addRole() {
-    inquirer
-      .prompt([
-        {
-          name: "title",
-          type: "input",
-          message: "add a role"
-        },
-        {
-          name: "salary",
-          type: "input",
-          message: "add a salary"
-        },
-        {
-          name: "department_id",
-          type: "input",
-          message: "Enter a department id"
+            break;
+
+          case "add a role":
+            inquirer
+              .prompt([
+                {
+                  name: "title",
+                  type: "input",
+                  message: "add a role"
+                },
+                {
+                  name: "salary",
+                  type: "input",
+                  message: "add a salary"
+                },
+                {
+                  name: "department_id",
+                  type: "input",
+                  message: "Enter a department id"
+                }
+              ])
+              .then(function(res) {
+                db.query(
+                  "INSERT INTO role (title, salary, department_id) VALUE (?, ?, ?)",
+                  [res.title, res.salary, res.department_id],
+                  function(err) {
+                    if (err) throw err;
+                    let user = new Functions();
+                    user.options();
+                  }
+                );
+              });
+
+            break;
+
+          case "add an employee":
+            inquirer
+              .prompt([
+                {
+                  name: "firstname",
+                  type: "input",
+                  message: "Enter first name"
+                },
+                {
+                  name: "lastname",
+                  type: "input",
+                  message: "Enter last name"
+                },
+                {
+                  name: "role_id",
+                  type: "input",
+                  message: "Enter role ID"
+                },
+                {
+                  name: "manager_id",
+                  type: "input",
+                  message: "Enter manager ID"
+                }
+              ])
+              .then(function(res) {
+                db.query(
+                  "INSERT INTO employee (firstname, lastname, role_id, manager_id) VALUES (?, ?, ?, ?)",
+                  [res.firstname, res.lastname, res.role_id, res.manager_id],
+                  err => {
+                    if (err) throw err;
+                    let user = new Functions();
+                    user.options();
+                  }
+                );
+              });
+
+            break;
+
+          case "view departments":
+            db.query("SELECT * FROM department", (err, res) => {
+              if (err) throw err;
+              const table = ctable.getTable(res);
+              console.log(table);
+              let user = new Functions();
+              user.options();
+            });
+
+            break;
+
+          case "view roles":
+            db.query("SELECT * FROM role", (err, res) => {
+              if (err) throw err;
+              const table = ctable.getTable(res);
+              console.log(table);
+              let user = new Functions();
+              user.options();
+            });
+
+            break;
+
+          case "view employees":
+            db.query("SELECT * FROM employee", (err, res) => {
+              if (err) throw err;
+              const table = ctable.getTable(res);
+              console.log(table);
+              let user = new Functions();
+              user.options();
+            });
+
+            break;
+
+          case "update employees":
+            inquirer
+              .prompt([
+                {
+                  name: "id",
+                  type: "input",
+                  message: "Enter ID of Employee you want to update"
+                },
+                {
+                  name: "firstname",
+                  type: "input",
+                  message: "Enter new first name"
+                },
+                {
+                  name: "lastname",
+                  type: "input",
+                  message: "Enter new last name"
+                },
+                {
+                  name: "role_id",
+                  type: "input",
+                  message: "Enter new role ID"
+                },
+                {
+                  name: "manager_id",
+                  type: "input",
+                  message: "Enter new manager ID"
+                }
+              ])
+              .then(function(res) {
+                db.query(
+                  "UPDATE employee SET firstname = ?, lastname = ?, role_id = ?, manager_id = ? WHERE id = ?",
+                  [
+                    res.firstname,
+                    res.lastname,
+                    res.role_id,
+                    res.manager_id,
+                    res.id
+                  ],
+                  err => {
+                    if (err) throw err;
+                    let user = new Functions();
+                    user.options();
+                  }
+                );
+              });
+
+            break;
+
+          case "update roles":
+            inquirer
+              .prompt([
+                {
+                  name: "id",
+                  type: "input",
+                  message: "Enter ID of role you want to update"
+                },
+                {
+                  name: "title",
+                  type: "input",
+                  message: "Enter title of role you want to update"
+                },
+                {
+                  name: "salary",
+                  type: "input",
+                  message: "Enter salary you want to update"
+                },
+                {
+                  name: "department_id",
+                  type: "input",
+                  message: "Enter ID of department you want to update"
+                }
+              ])
+              .then(function(res) {
+                db.query(
+                  "UPDATE role SET title = ?, salary = ?, department_id = ? WHERE id = ?",
+                  [res.title, res.salary, res.department_id, res.id],
+                  err => {
+                    if (err) throw err;
+                    let user = new Functions();
+                    user.options();
+                  }
+                );
+              });
+
+            break;
+          case "exit":
+            process.exit();
+            break;
         }
-      ])
-      .then(function(res) {
-        conn.query(
-          "INSERT INTO role (title, salary, department_id) VALUE (?, ?, ?)",
-          [res.title, res.salary, res.department_id],
-          function(err) {
-            if (err) throw err;
-            options();
-          }
-        );
-      });
-  }
-
-  addEmployee() {
-    inquirer
-      .prompt([
-        {
-          name: "firstname",
-          type: "input",
-          message: "Enter first name"
-        },
-        {
-          name: "lastname",
-          type: "input",
-          message: "Enter last name"
-        },
-        {
-          name: "role_id",
-          type: "input",
-          message: "Enter role ID"
-        },
-        {
-          name: "manager_id",
-          type: "input",
-          message: "Enter manager ID"
-        }
-      ])
-      .then(function(res) {
-        conn.query(
-          "INSERT INTO employee (firstname, lastname, role_id, manager_id) VALUES (?, ?, ?, ?)",
-          [res.firstname, res.lastname, res.role_id, res.manager_id],
-          err => {
-            if (err) throw err;
-            options();
-          }
-        );
-      });
-  }
-
-  viewRoles() {
-    conn.query("SELECT * FROM role", (err, res) => {
-      if (err) throw err;
-      const table = ctable.getTable(res);
-      console.log(table);
-      options();
-    });
-  }
-
-  viewDepartments() {
-    conn.query("SELECT * FROM department", (err, res) => {
-      if (err) throw err;
-      const table = ctable.getTable(res);
-      console.log(table);
-      options();
-    });
-  }
-
-  viewEmployees() {
-    conn.query("SELECT * FROM employee", (err, res) => {
-      if (err) throw err;
-      const table = ctable.getTable(res);
-      console.log(table);
-      options();
-    });
-  }
-
-  updateEmployees() {
-    inquirer
-      .prompt([
-        {
-          name: "firstname",
-          type: "input",
-          message: "Enter first name of employee you want to update"
-        },
-        {
-          name: "lastname",
-          type: "input",
-          message: "Enter last name of employee you want to update"
-        },
-        {
-          name: "role_id",
-          type: "input",
-          message: "Enter ID of role you want to update"
-        },
-        {
-          name: "manager_id",
-          type: "input",
-          message: "Enter manager ID of you want to update "
-        }
-      ])
-      .then(function(res) {
-        conn.query(
-          "UPDATE employee SET firstname = ?, lastname = ?, role_id = ?, manager_id = ?",
-          [res.firstname, res.lastname, res.role_id, res.manager_id],
-          err => {
-            if (err) throw err;
-            options();
-          }
-        );
-      });
-  }
-
-  updateRoles() {
-    inquirer
-      .prompt([
-        {
-          name: "title",
-          type: "input",
-          message: "Enter title of role you want to update"
-        },
-        {
-          name: "salary",
-          type: "input",
-          message: "Enter salary you want to update"
-        },
-        {
-          name: "department_id",
-          type: "input",
-          message: "Enter ID of department you want to update"
-        }
-      ])
-      .then(function(res) {
-        connection.query(
-          "UPDATE role SET title = ?, salary = ?, department_id = ? WHERE id = ?",
-          [res.title, res.salary, res.department_id],
-          err => {
-            if (err) throw err;
-            options();
-          }
-        );
       });
   }
 }
 
-module.exports = Functions;
+module.exports = new Functions();
